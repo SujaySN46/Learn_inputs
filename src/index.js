ddf50b28541c64496d0fde542cd0fe8bb7b78284
Sujay S N <80213368+SujaySN46@users.noreply.github.com>
2021-03-10 13:02:38 +0530

    add css

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <div>
-    <h1>hello world!</h1>
+  <div style={{ margin: 10 + "px" }}>
+    <h1 style={{ color: "red" }}>hello world!</h1>
     <p>this is a paragraph</p>
     <br />
     <label>name</label>
+    <br />
     <input type="text" name="input1"></input>
     <br />
     <label>Password</label>
+    <br />
     <input type="text" name="password"></input>
     <br />
-    <input type="submit"></input>
+    <input type="submit" style={{ margin: 10 + "px" }}></input>
   </div>,
   document.getElementById("root")
 );
